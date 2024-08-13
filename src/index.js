ec087a9a464257b48d508c6328532e8c3e913dcc
Jean Caiza <jeanmcm@hotmail.com>
2024-08-12 21:37:27 -0500

    w

--- src/index.js
+++ src/index.js
@@ -1,132 +1,67 @@
-import playwright from "playwright";
 import http from "http";
-import { writeFile } from "fs/promises";
+import { writeFile, readFile } from "fs/promises";
 
-const getRows = async (table) => {
-  const rowsSelector = await table.locator("tbody > tr").all();
-
-  const rows = [];
-  for (const row of rowsSelector) {
-    const cells = await row.locator("td").all();
-    const rowData = [];
-    for (const cell of cells) {
-      const text = await cell.textContent();
-      rowData.push(text);
-    }
-    rows.push(rowData);
-  }
-
-  return rows;
-};
-const main = async () => {
-  /**
-   * @type {playwright.LaunchOptions}
-   */
-  const launchOptions = {
-    headless: false,
-  };
-  const browser = await playwright.firefox.launch(launchOptions);
-
-  const page = await browser.newPage();
-  await page.goto("https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT");
-
-  await page.getByText("Iniciar sesión").first().click();
-
-  const rucInput = page.getByPlaceholder("1700000000001");
-
-  await rucInput.fill(process.env.RUC);
-
-  const passInput = page.getByPlaceholder("Clave");
-
-  await passInput.fill(process.env.PASSWORD);
-
-  const loginButton = page.getByText("Ingresar");
-
-  await loginButton.click();
-
-  await page.waitForTimeout(1000);
-
-  const btnInvoice = page.getByTitle("Facturación Electrónica");
-  await btnInvoice.click();
-
-  const subBtn = page.getByText("Comprobantes electrónicos recibidos");
-  await subBtn.click();
-
-  await page.waitForSelector('[id="frmPrincipal:ano"]');
-
-  // const yearCb = page.locator('[id="frmPrincipal:ano"]')
-  // const monthCb = page.locator('[id="frmPrincipal:mes"]')
-  const dayCb = page.locator('[id="frmPrincipal:dia"]');
-  await dayCb.click();
-  await dayCb.selectOption("0");
-
-  await page.waitForTimeout(100);
-
-  const submitBtn = page.getByRole("button", { name: "Consultar" });
-
-  await submitBtn.click();
-
-  await page.waitForSelector('[id="frmPrincipal:tablaCompRecibidos"]');
-
-  try {
-    const table = page.locator('[id="frmPrincipal:tablaCompRecibidos"] > table');
-    const footer = table.locator("tfoot > tr > td");
-
-    const headersSelector = await table.locator("thead > tr > th").all();
-
-    const paginatorSelect = footer.locator("select");
-    await paginatorSelect.selectOption("75");
-
-    const getPaginatorValues = async () => {
-      const currentPaginator = footer.locator(".ui-paginator-current");
-      const paginatorValues = await currentPaginator.textContent(); //(1 of 1)
-      const match = paginatorValues.match(/\((\d+)\s+of\s+(\d+)\)/);
-      const current = parseInt(match[1], 10);
-      let total = parseInt(match[2], 10);
-
-      return { current, total };
-    };
-
-    const rows = [...(await getRows(table))];
-
-    let paginatorValues = await getPaginatorValues();
-    const nextBtn = footer.locator(".ui-paginator-next");
-
-    while (paginatorValues.current < paginatorValues.total) {
-      await nextBtn.click();
-      paginatorValues = await getPaginatorValues();
-      await page.waitForTimeout(100);
-
-      rows.push(...(await getRows(table)));
-    }
-
-    const keys = await Promise.all(headersSelector.map((e) => e.textContent()));
-    const result = [];
-
-    for (const row of rows) {
-      const obj = {};
-      for (let i = 0; i < row.length; i++) {
-        obj[keys[i]] = row[i];
-      }
-      result.push(obj);
-    }
-
-    await writeFile("data.json", JSON.stringify(result));
-    console.log("Data saved");
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-main();
+import { getInvoices } from "./invoices.js";
+import supabase from "./database.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
-  if (method === "POST" && url === "/") {
-    res.end("OK");
+  if (method === "GET" && url === "get") {
+    const invoices = await getInvoices();
+    await writeFile("data.json", JSON.stringify(invoices));
   }
 });
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(+PORT, () => {
   console.log("Server running on port 3000");
 });
+
+const lookUp = {
+  "RUC y Razón social emisor": ["ruc", "entity"],
+  "Tipo y serie de comprobante": "type_serie",
+  "Clave de acceso / Nro. Autorización": "access_key",
+  "Fecha y hora de autorización": "authorization_date",
+  "Fecha emisión": "emit_date",
+  "Valor sin impuestos": "value",
+  IVA: "tax",
+  "Importe Total": "total",
+};
+
+const main = async () => {
+  const invoices = await readFile("data.json", "utf-8");
+  const data = JSON.parse(invoices);
+  const result = data.map((item) => {
+    const newObj = {};
+    for (const [key, value] of Object.entries(item)) {
+      const newKey = lookUp[key];
+      if (newKey) {
+        if (Array.isArray(newKey)) {
+          const ruc = value.slice(0, 13);
+          const entity = value.slice(13).trim();
+
+          newObj[newKey[0]] = ruc;
+          newObj[newKey[1]] = entity;
+        } else {
+          newObj[newKey] = ["value", "tax", "total"].includes(newKey)
+            ? parseFloat(value)
+            : newKey.includes("date")
+            ? new Date(value).toISOString().slice(0, 22)
+            : value;
+        }
+      }
+    }
+
+    return newObj;
+  });
+
+  const r = await supabase.from("invoices").select();
+  console.log(r);
+  // console.log(result);
+  // const r = await supabase.from("invoices").insert(result);
+
+  // console.log(r);
+};
+
+main();
---
+++ src/invoices.js
@@ -0,0 +1,116 @@
+import playwright from "playwright";
+
+const getRows = async (table) => {
+  const rowsSelector = await table.locator("tbody > tr").all();
+
+  const rows = [];
+  for (const row of rowsSelector) {
+    const cells = await row.locator("td").all();
+    const rowData = [];
+    for (const cell of cells) {
+      const text = await cell.textContent();
+      rowData.push(text);
+    }
+    rows.push(rowData);
+  }
+
+  return rows;
+};
+export const getInvoices = async () => {
+  /**
+   * @type {playwright.LaunchOptions}
+   */
+  const launchOptions = {
+    headless: false,
+  };
+  const browser = await playwright.firefox.launch(launchOptions);
+
+  const page = await browser.newPage();
+  await page.goto("https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT");
+
+  await page.getByText("Iniciar sesión").first().click();
+
+  const rucInput = page.getByPlaceholder("1700000000001");
+
+  await rucInput.fill(process.env.RUC);
+
+  const passInput = page.getByPlaceholder("Clave");
+
+  await passInput.fill(process.env.PASSWORD);
+
+  const loginButton = page.getByText("Ingresar");
+
+  await loginButton.click();
+
+  await page.waitForTimeout(1000);
+
+  const btnInvoice = page.getByTitle("Facturación Electrónica");
+  await btnInvoice.click();
+
+  const subBtn = page.getByText("Comprobantes electrónicos recibidos");
+  await subBtn.click();
+
+  await page.waitForSelector('[id="frmPrincipal:ano"]');
+
+  // const yearCb = page.locator('[id="frmPrincipal:ano"]')
+  // const monthCb = page.locator('[id="frmPrincipal:mes"]')
+  const dayCb = page.locator('[id="frmPrincipal:dia"]');
+  await dayCb.click();
+  await dayCb.selectOption("0");
+
+  await page.waitForTimeout(100);
+
+  const submitBtn = page.getByRole("button", { name: "Consultar" });
+
+  await submitBtn.click();
+
+  await page.waitForSelector('[id="frmPrincipal:tablaCompRecibidos"]');
+
+  try {
+    const table = page.locator('[id="frmPrincipal:tablaCompRecibidos"] > table');
+    const footer = table.locator("tfoot > tr > td");
+
+    const headersSelector = await table.locator("thead > tr > th").all();
+
+    const paginatorSelect = footer.locator("select");
+    await paginatorSelect.selectOption("75");
+
+    const getPaginatorValues = async () => {
+      const currentPaginator = footer.locator(".ui-paginator-current");
+      const paginatorValues = await currentPaginator.textContent(); //(1 of 1)
+      const match = paginatorValues.match(/\((\d+)\s+of\s+(\d+)\)/);
+      const current = parseInt(match[1], 10);
+      let total = parseInt(match[2], 10);
+
+      return { current, total };
+    };
+
+    const rows = [...(await getRows(table))];
+
+    let paginatorValues = await getPaginatorValues();
+    const nextBtn = footer.locator(".ui-paginator-next");
+
+    while (paginatorValues.current < paginatorValues.total) {
+      await nextBtn.click();
+      paginatorValues = await getPaginatorValues();
+      await page.waitForTimeout(100);
+
+      rows.push(...(await getRows(table)));
+    }
+
+    const keys = await Promise.all(headersSelector.map((e) => e.textContent()));
+    const result = [];
+
+    for (const row of rows) {
+      const obj = {};
+      for (let i = 0; i < row.length; i++) {
+        obj[keys[i]] = row[i];
+      }
+      result.push(obj);
+    }
+
+    return result;
+  } catch (error) {
+    return [];
+  }
+};
