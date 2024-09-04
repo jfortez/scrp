170be363002c47a3d35b2beeecb209ebc7236729
Jean Caiza <jeanmcm@hotmail.com>
2024-09-04 09:11:35 -0500

    split

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,9 @@
 import http from "http";
-import { writeFile, readFile } from "fs/promises";
 
 import { getInvoices } from "./invoices";
 import supabase from "./database";
 import { Tables } from "./database.types";
-
-const server = http.createServer(async (req, res) => {
-  const { method, url } = req;
-  if (method === "GET" && url === "get") {
-    const invoices = await getInvoices();
-    await writeFile("data.json", JSON.stringify(invoices));
-  }
-});
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(+PORT, () => {
-  console.log("Server running on port 3000");
-});
+import { writeFile } from "fs/promises";
 
 const lookUp: Record<string, string | string[]> = {
   "RUC y Razón social emisor": ["ruc", "entity"],
@@ -30,49 +16,66 @@ const lookUp: Record<string, string | string[]> = {
   "Importe Total": "total",
 };
 
-type InvoiceJSON = {
-  "RUC y Razón social emisor": string;
-  "Tipo y serie de comprobante": string;
-  "Clave de acceso / Nro. Autorización": string;
-  "Fecha y hora de autorización": string;
-  "Fecha emisión": string;
-  "Valor sin impuestos": string;
-  IVA: string;
-  "Importe Total": string;
-};
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+  if (method === "GET") {
+    if (url === "/load-invoices") {
+      try {
+        const invoices = await getInvoices();
+        await writeFile("data.json", JSON.stringify(invoices));
 
-const main = async () => {
-  const invoices = await readFile("data.json", "utf-8");
-  const data = JSON.parse(invoices) as InvoiceJSON[];
-  const result = data.map((item) => {
-    const newObj: Record<string, any> = {};
-    for (const [key, value] of Object.entries(item)) {
-      const newKey = lookUp[key];
-      if (newKey) {
-        if (Array.isArray(newKey)) {
-          const ruc = value.slice(0, 13);
-          const entity = value.slice(13).trim();
+        const data = invoices.map((item) => {
+          const newObj: Record<string, any> = {};
+          for (const [key, value] of Object.entries(item)) {
+            const newKey = lookUp[key];
+            if (newKey) {
+              if (Array.isArray(newKey)) {
+                const ruc = value.slice(0, 13);
+                const entity = value.slice(13).trim();
 
-          newObj[newKey[0]] = ruc;
-          newObj[newKey[1]] = entity;
-        } else {
-          newObj[newKey] = ["value", "tax", "total"].includes(newKey)
-            ? parseFloat(value)
-            : newKey.includes("date")
-            ? new Date(value).toISOString()
-            : // ? new Date(value).toISOString().slice(0, 22)
+                newObj[newKey[0]] = ruc;
+                newObj[newKey[1]] = entity;
+              } else {
+                newObj[newKey] = ["value", "tax", "total"].includes(newKey)
+                  ? parseFloat(value)
+                  : newKey.includes("date")
+                  ? new Date(value).toISOString()
+                  : // ? new Date(value).toISOString().slice(0, 22)
 
-              value;
-        }
+                    value;
+              }
+            }
+          }
+
+          return newObj;
+        }) as Tables<"invoices">[];
+        const result = await supabase.from("invoices").insert(data);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(result));
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(error));
       }
     }
 
-    return newObj;
-  }) as Tables<"invoices">[];
+    if (url === "/invoices") {
+      try {
+        const invoices = await supabase.from("invoices").select("*");
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(invoices));
+      } catch (error) {
+        res.writeHead(500, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(error));
+      }
+    }
+  } else {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  }
+});
 
-  // const r = await supabase.from("invoices").select("*");
+const PORT = process.env.PORT || 3000;
 
-  // const m = await supabase.from("invoices").insert(result);
-};
-
-main();
+server.listen(+PORT, () => {
+  console.log("Server running on port", PORT);
+});
--- src/invoices.ts
+++ src/invoices.ts
@@ -1,4 +1,15 @@
-import playwright, { type Locator } from "playwright";
+import playwright, { type Locator, type Page } from "playwright";
+
+type InvoiceJSON = {
+  "RUC y Razón social emisor": string;
+  "Tipo y serie de comprobante": string;
+  "Clave de acceso / Nro. Autorización": string;
+  "Fecha y hora de autorización": string;
+  "Fecha emisión": string;
+  "Valor sin impuestos": string;
+  IVA: string;
+  "Importe Total": string;
+};
 
 const getRows = async (table: Locator) => {
   const rowsSelector = await table.locator("tbody > tr").all();
@@ -16,57 +27,19 @@ const getRows = async (table: Locator) => {
 
   return rows;
 };
-export const getInvoices = async () => {
-  /**
-   * @type {playwright.LaunchOptions}
-   */
-  const launchOptions = {
-    headless: false,
-  };
-  const browser = await playwright.firefox.launch(launchOptions);
 
-  const page = await browser.newPage();
-  await page.goto("https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT");
-
-  await page.getByText("Iniciar sesión").first().click();
-
-  const rucInput = page.getByPlaceholder("1700000000001");
-
-  await rucInput.fill(process.env.RUC);
-
-  const passInput = page.getByPlaceholder("Clave");
-
-  await passInput.fill(process.env.PASSWORD);
-
-  const loginButton = page.getByText("Ingresar");
-
-  await loginButton.click();
-
-  await page.waitForTimeout(1000);
-
-  const btnInvoice = page.getByTitle("Facturación Electrónica");
-  await btnInvoice.click();
-
-  const subBtn = page.getByText("Comprobantes electrónicos recibidos");
-  await subBtn.click();
-
-  await page.waitForSelector('[id="frmPrincipal:ano"]');
-
-  // const yearCb = page.locator('[id="frmPrincipal:ano"]')
-  // const monthCb = page.locator('[id="frmPrincipal:mes"]')
-  const dayCb = page.locator('[id="frmPrincipal:dia"]');
-  await dayCb.click();
-  await dayCb.selectOption("0");
-
-  await page.waitForTimeout(100);
-
-  const submitBtn = page.getByRole("button", { name: "Consultar" });
-
-  await submitBtn.click();
-
-  await page.waitForSelector('[id="frmPrincipal:tablaCompRecibidos"]');
-
+const getTableData = async (page: Page): Promise<InvoiceJSON[]> => {
   try {
+    const submitBtn = page.getByRole("button", { name: "Consultar" });
+
+    await submitBtn.click();
+
+    const noDataMessage = page.locator('[id="formMessages:messages"]');
+    if (await noDataMessage.isVisible()) {
+      return [];
+    }
+
+    await page.waitForSelector('[id="frmPrincipal:tablaCompRecibidos"]');
     const table = page.locator('[id="frmPrincipal:tablaCompRecibidos"] > table');
     const footer = table.locator("tfoot > tr > td");
 
@@ -99,12 +72,16 @@ export const getInvoices = async () => {
     }
 
     const keys = await Promise.all(headersSelector.map((e) => e.textContent()));
-    const result = [];
+    const result: InvoiceJSON[] = [];
 
     for (const row of rows) {
-      const obj = {} as Record<string, string>;
+      const obj: InvoiceJSON = {} as InvoiceJSON;
       for (let i = 0; i < row.length; i++) {
-        obj[keys[i] as string] = row[i] as string;
+        const key = keys[i] as keyof InvoiceJSON;
+        const value = row[i];
+        if (key && value) {
+          obj[key] = value;
+        }
       }
       result.push(obj);
     }
@@ -113,4 +90,64 @@ export const getInvoices = async () => {
   } catch (error) {
     return [];
   }
+};
+export const getInvoices = async () => {
+  const launchOptions: playwright.LaunchOptions = {
+    headless: false,
+  };
+  const browser = await playwright.firefox.launch(launchOptions);
+
+  const page = await browser.newPage();
+  await page.goto("https://srienlinea.sri.gob.ec/sri-en-linea/inicio/NAT");
+
+  await page.getByText("Iniciar sesión").first().click();
+
+  const rucInput = page.getByPlaceholder("1700000000001");
+
+  await rucInput.fill(process.env.RUC);
+
+  const passInput = page.getByPlaceholder("Clave");
+
+  await passInput.fill(process.env.PASSWORD);
+
+  const loginButton = page.getByText("Ingresar");
+
+  await loginButton.click();
+
+  await page.waitForTimeout(1000);
+
+  const btnInvoice = page.getByTitle("Facturación Electrónica");
+  await btnInvoice.click();
+
+  const subBtn = page.getByText("Comprobantes electrónicos recibidos");
+  await subBtn.click();
+
+  await page.waitForSelector('[id="frmPrincipal:ano"]');
+
+  const yearCb = page.locator('[id="frmPrincipal:ano"]');
+  const monthCb = page.locator('[id="frmPrincipal:mes"]');
+  const dayCb = page.locator('[id="frmPrincipal:dia"]');
+  await dayCb.click();
+  await dayCb.selectOption("0");
+
+  const yearOptions = ["2020", "2021", "2022", "2023", "2024"];
+  const monthOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"];
+
+  let invoices: InvoiceJSON[] = [];
+
+  for (const year of yearOptions) {
+    await yearCb.selectOption(year);
+    for (const month of monthOptions) {
+      await monthCb.selectOption(month);
+      await page.waitForTimeout(100); // Pequeña pausa para asegurar que la página se actualice
+
+      const tableData = await getTableData(page);
+      invoices = invoices.concat(tableData);
+    }
+  }
+
+  // Aquí puedes usar allTableData que contendrá todos los datos recopilados
+  console.log(`Total de facturas recopiladas: ${invoices.length}`);
+
+  return invoices;
 };
